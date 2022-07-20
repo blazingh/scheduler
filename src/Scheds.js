afbe4f136490bb246d0df3e77ae03d2b9d585167
blazingh <hadibaalbaki2@gmail.com>
2022-07-20 23:26:49 +0300

    added local storage functions

--- src/Scheds.js
+++ src/Scheds.js
@@ -1,29 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Scheds = () => {
-	const scheds = [
-		{ id: 1, name: "summer schedule", today: "4", all: "20" },
-		{ id: 2, name: "teacher schedule", today: "6", all: "29" },
-		{ id: 3, name: "coach schedule", today: "3", all: "15" },
-	];
+	const [schedules, setSchedules] = useState([]);
+	const handleNewSched = () => {
+		const schedId = schedules.length + 1;
+		setSchedules((current) => [
+			...current,
+			{ id: schedId, name: `New Schedule ${schedId}`, events: [] },
+		]);
+	};
+
+	useEffect(() => {
+		if (schedules.length > 0) {
+			localStorage.setItem("data", JSON.stringify(schedules));
+		}
+	}, [schedules]);
+	useEffect(() => {
+		setSchedules(JSON.parse(localStorage.getItem("data")));
+	}, []);
 	return (
 		<div className="flex flex-col justify-center items-center text-light">
 			<div className="w-full bg-dark flex justify-center items-center py-3">
 				<label className="text-xl font-semibold">Schedules</label>
 			</div>
 			<div className="flex flex-col justify-center items-center w-full max-w-md px-3">
-				{scheds.map((sched) => (
+				{schedules.map((sched) => (
 					<div className="sched-container" key={sched.id}>
 						<label className=" col-span-2 text-center mb-3 text-lg font-semibold ">
 							{sched.name}
 						</label>
 						<label className=" text-center font-semibold">
 							Today's Events{" "}
-							<label className=" text-primary-100"> {sched.today}</label>
+							<label className=" text-primary-100">{sched.events.length}</label>
 						</label>
 						<label className=" text-center font-semibold ">
-							<label className=" text-primary-100"> {sched.all}</label> Total
-							Events
+							<label className=" text-primary-100">{sched.events.length}</label>{" "}
+							Total Events
 						</label>
 					</div>
 				))}
@@ -39,7 +51,9 @@ const Scheds = () => {
 						</label>
 					</div>
 					<div className="peer-checked:flex hidden items-center col-span-2 row-span-2 justify-around ">
-						<label className="button-style">New Schedule</label>
+						<label className="button-style" onClick={handleNewSched}>
+							New Schedule
+						</label>
 						<div className="flex relative">
 							<label
 								htmlFor="list"
@@ -49,8 +63,11 @@ const Scheds = () => {
 							</label>
 							<input type="checkbox" className=" peer hidden" id="list" />
 							<div className="bg-secondary-100 absolute peer-checked:flex hidden z-10 right-0 top-7 w-40  text-white rounded flex-col justify-center items-center rounded-tr-none">
-								{scheds.map((sched) => (
-									<label className="py-1 cursor-pointer hover:bg-secondary-200 w-full text-center">
+								{schedules.map((sched) => (
+									<label
+										key={sched.id}
+										className="py-1 cursor-pointer hover:bg-secondary-200 w-full text-center"
+									>
 										{sched.name}
 									</label>
 								))}
